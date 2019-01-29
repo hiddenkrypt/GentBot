5562cf31e07bf7399055310fc34985c952481cff
Rav-T <hiddenkrypt@gmail.com>
2019-01-29 12:46:24 -0800

    first bot connection

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+require("dotenv").config();
+
 console.log("NODE START");
 console.log(process.env.DISCORD_BOT_PREFIX);
+
+const DiscordApp = require("discord.js");
+const client = new DiscordApp.Client();
+
+client.on('ready', ()=>{
+  console.log("Connected! Tag: "+client.user.tag);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+
+
+//GGG Botspam channel ID 503362277844451338
