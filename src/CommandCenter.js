98666d20f962056993017d45a64debbc857cdb1f
Rav-T <hiddenkrypt@gmail.com>
2019-01-29 18:52:36 -0800

    continued shaping of bones

--- src/CommandCenter.js
+++ src/CommandCenter.js
@@ -1,23 +1,21 @@
 module.exports = {
-  
-  process: function(msg){
 
+  process: function(msg){
+    //parse args, sent to specific command function
   },
 
 
   dm: function(msg){
+    let pornTerms = ["nsfw", "lewd", "porn", "p0rn", "pron", "pr0n", "nfsw"];
     let addTerms = ["add","want","give","gimme","desire","need","wish","crave","yearn" ,"provide","allow","access","grant","provide","bestow"];
     let removeTerms = ["remove","take","delete","erase","extract","exclude","snag","stop","no"];
-    if(msg.content.includes("nsfw")){
-      if (addTerms.find(e=>{msg.content.contains(e);})){
-        //give this user nsfw role on GGG
+    if(pornTerms.find(e=>{return msg.content.includes(e);})){
+      if (addTerms.find(e=>{return msg.content.includes(e);})){
         console.log(`${msg.author} wants NSFW permission.`);
-        //guild.roles <-- (collection)
-        //Guild.fetchMember(<user>) <-- promise
-        //guild member.addRole ("<roleID>", "<reason>"); <-- promise
+        grantNSFW(msg.author);
       }
-      else if (removeTerms.find(e=>{msg.content.contains(e);})){
-        console.log(`${msg.author} DOES NOT WANT NSFW permission.`);
+      else if (removeTerms.find(e=>{return msg.content.includes(e);})){
+        console.log(`${msg.author} wants to remove NSFW permission.`);
       }
       else {
         msg.channel.send("If you want the NSFW permission on the GGG, say something like 'I want NSFW'");
@@ -25,3 +23,12 @@ module.exports = {
     }
   }
 };
+
+
+function grantNSFW() {
+
+  //give this user nsfw role on GGG
+  //guild.roles <-- (collection)
+  //Guild.fetchMember(<user>) <-- promise
+  //guild member.addRole ("<roleID>", "<reason>"); <-- promise
+}
